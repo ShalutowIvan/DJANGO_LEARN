745682bd4915fa3b1c1b722d4bf4eb558776a308
Иван <ivan_shalutow@mail.ru>
2023-07-10 21:52:34 +0700

    asd

--- Mysite/Mysite/settings.py
+++ Mysite/Mysite/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'#это уже было указано по умолчанию
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')#это путь к общей папке static
+STATICFILES_DIRS = []#список нестадартных путей пока пустой
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
