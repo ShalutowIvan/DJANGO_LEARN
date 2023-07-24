a068194e59893644cdfbbc8be132a7796e8f81f1
Иван <ivan_shalutow@mail.ru>
2023-07-24 20:55:34 +0700

    asd

--- Mysite/home/apps.py
+++ Mysite/home/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
+#это класс применяется для конфигурации всего приложения home
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+    verbose_name = "Самые крутые дома всего мира"#это название приложения в админке. Также можно его использовать в разных модулях джанго
