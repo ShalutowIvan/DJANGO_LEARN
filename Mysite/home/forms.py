7119837f54c06d566a2d690fcf7cf90619176be0
Иван <ivan_shalutow@mail.ru>
2023-08-22 01:57:22 +0700

    asd

--- Mysite/home/forms.py
+++ Mysite/home/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import *
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 # class AddPostForm(forms.Form):#написали название класса (его можно писать любое) и родителя указали класс Form
 #     title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))#далее тут идут поля которые будут отображаться в нашей форме. Их нужно будет заполнять пользователю. Эти атрибуты совпадают с названиеми полей в модели БД, так как потом эти данные будут попадать в БД. Названия лучше делать такими же как в модели, чтобы облегчить написание кода.
@@ -37,4 +38,31 @@ class AddPostForm(forms.ModelForm):#унаследуем другой базов
             raise ValidationError('Длина превышает 200 символов')#ValidationError нужно также импортировать из django.core.exceptions 
         #в случае если проверка пройдет, то просто возвращается заголовок
         return title
+
+
+class RegisterUserForm(UserCreationForm):#делаем класс для формы на базе UserCreationForm, его также нужно импортировать.
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+
+
+
+    class Meta:
+        model = User#пишем модель. Ее также нужно импортировать из джанго
+        fields = ('username', 'email', 'password1', 'password2')#указали поля для нашей формы.
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-input'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
+        }#виджеты для назначение классов для полей, то есть стили добавлеяем через эти классы. Имена полей можно посмотреть через панель разработчика в браузере через просмотреть код на нужном нам элементе. Там есть input поля и прописаны имена полей. Они также берутся из джанго. Также можно посмотреть и имена полей для других полей, не только username и пароли. Делаем мы так, потому что мы делаем свою форму вместо стандартной. В другом фреймворке наверно можно и свои поля для своей формы указать. Далее перейдем в файл views.py и перепишем наш класс. 
+        #стили для стандартных полей почему-то не работают. Поэтому нам придется переопределить эти поля и к ним дописать также виджеты со стилями. Переопределить их нужно до класса Meta. Теперь стили будут работать. Перейдем далее в шаблон и перепишем там шаблон. Добавим еще поле email в нашу форму. Теперь можно регать новых пользователей. Выглядит совсем все стандартно.  
+
+
+class LoginUserForm(AuthenticationForm):#название сами придумали, базовый класс нужно также импортировать дополинетльно. 
+    #ниже мы определели поля для ввода логина и пароля и прописали к ним виджеты для стилей css. 
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    #если нужны еещ другие поля для авторизации, то их тут также нужно прописать. Эту форму также укажем в views.py.  
+
 
